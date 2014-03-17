bb5e7390067e96da3f69810984bb8d6bcd1e5aef
Brian Deane <bdeane@thrillist.com>
2014-03-17 10:46:26 -0400

    Better naming

--- popup.js
+++ popup.js
@@ -1,11 +1,10 @@
 var clippy = {
-	addStuffToClipboard: function() {
+	addEmailToPage: function() {
 		var text = "test_" + Date.now() + "@mailinator.com";
 		$('input').val(text);
 	}
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
 $(document).ready( function () {
-	clippy.addStuffToClipboard();
+	clippy.addEmailToPage();
 });
